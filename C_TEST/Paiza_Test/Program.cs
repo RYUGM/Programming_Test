105a0445d6402d596fcd3637e5e1ffe36c69dad7
류경문 <92001666+RYUGM@users.noreply.github.com>
2024-01-16 23:12:38 +0900

    2024‐しりとり
    2024‐しりとり

--- C_TEST/Paiza_Test/Program.cs
+++ C_TEST/Paiza_Test/Program.cs
@@ -1,13 +1,52 @@
-﻿
-using System;
-using static Example;
-
-public class Example
+﻿using System;
+class Program
 {
-    //최소 공약
-
-    public static void Main()
+    static void Main()
     {
-        Console.WriteLine("asd");
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int line_cnt = int.Parse(Console.ReadLine());
+
+        int cnt = 0;
+        string temp_1 = "";
+        string temp_2 = "";
+
+        string resutl = "";
+        temp_1 = Console.ReadLine();
+        while (true)
+        {
+
+
+            if (cnt != 0)
+            {
+                temp_2 = Console.ReadLine();
+
+                if (temp_1.Substring(temp_1.Length - 1, 1) == temp_2.Substring(0, 1))
+                {
+                    temp_1 = temp_2;
+                    temp_2 = "";
+                    resutl = "yes";
+                }
+                else if (temp_1.Substring(temp_1.Length - 1, 1) != temp_2.Substring(0, 1))
+                {
+                    resutl = temp_1.Substring(temp_1.Length - 1, 1) + " " + temp_2.Substring(0, 1);
+                    break;
+                }
+            }
+
+
+
+
+            cnt++;
+            if (cnt == line_cnt)
+            {
+                resutl = "Yes";
+                break;
+            }
+
+        }
+
+        Console.WriteLine(resutl);
+
     }
 }
