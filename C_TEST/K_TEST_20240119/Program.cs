d3db6b9af2fdf8f05d9f9502c65b85c239290d2d
GMRYU <newruh2@gmail.com>
2024-01-19 17:11:25 +0900

    20240119_K_TEST
    20240119_K_TEST

---
+++ C_TEST/K_TEST_20240119/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int solution(int num)
+    {
+        int answer = 0;
+
+        int input_num = num;
+        int result_num = 0;
+        int cnt = 0;
+
+        while (input_num != 1)
+        {
+            if (cnt >= 500) //int 범위 벗어나는 경우 있음...어이가 없네 ㅋㅋㅋ
+            {
+                return -1;
+            }
+            if (input_num % 2 == 0)
+            {
+                input_num = input_num / 2;
+
+            }
+            else
+            {
+                input_num = input_num * 3 + 1;
+            }
+            cnt++;
+
+        }
+
+
+        return cnt;
+    }
+}
---
+++ C_TEST/K_TEST_20240119_divide/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_TEST_20240119_divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] answer = new int[5] ;
+            answer[0] = 5;
+            string temp = "";
+            int temp_int = 0;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (answer[i] % 1 == 0)
+                {
+                    temp += answer[i] + " ";
+                    temp_int++;
+                }
+            }
+
+            string[] temp_result = temp.Substring(0, temp.Length).Split(' ');
+            int[] temp_result_int = new int[1];
+            temp_result_int[0] = int.Parse(temp_result[0]);
+
+            Console.WriteLine(temp_result_int[0]+"");
+
+        }
+    }
+}
