2c5dc25a92f25ab68139caa0ff93f709ea840dad
GMRYU <newruh2@gmail.com>
2024-01-24 17:53:44 +0900

    20240124
    20240124

---
+++ C_TEST/K_TEST_20240124_Ascii/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_TEST_20240124_Ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp_s = "z";
+            char temp_c = char.Parse (temp_s.Substring(0, 1));
+            int temp_i = (int)temp_c;
+            char temp_xx = (char)32;
+
+            if (char.IsUpper((char)temp_i))
+            {
+                Console.WriteLine((char)temp_i + "B" + temp_i+"/" + (char)(64 + (94 - 90)));
+            }
+            else {
+                Console.WriteLine((char)temp_i + "S"+ temp_i + "/" + (char)(96+((122+25)-122)));
+            }
+
+           
+
+        }
+        static string solution(string s, int n)
+        {
+            string answer = "";
+
+            char temp = (char)32;
+            int temp_int = 0;
+            char[] char_array = new char[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp = char.Parse(s.Substring(i, 1));
+                temp_int = (int)temp;
+                if (temp_int == 32)
+                { //공백
+                    char_array[i] = (char)temp_int;
+                }
+                else if (char.IsUpper(temp))
+                {
+                    //대문자일때
+                    if (temp_int + n > 90)
+                    {
+                        temp_int = (64 + ((temp_int + n) - 90));
+                        char_array[i] = (char)temp_int;
+                        //A = 65 + 90/temp_int+n
+
+                    }
+                    else
+                    {
+                        temp_int = temp_int + n;
+                        char_array[i] = (char)temp_int;
+                    }
+
+
+                }
+                else if (char.IsLower(temp))
+                {
+                    //소문자일때
+                    if (temp_int + n > 122)
+                    {
+                        //a = 97
+                        temp_int = (96 + ((temp_int + n) - 122));
+                        char_array[i] = (char)temp_int;
+                    }
+                    else
+                    {
+                        temp_int = temp_int + n;
+                        char_array[i] = (char)temp_int;
+                    }
+
+
+
+                }
+
+            }
+
+            string result_string = "";
+            for (int i = 0; i < char_array.Length; i++)
+            {
+
+
+                result_string += char_array[i].ToString();
+            }
+
+
+            return result_string;
+        }
+    }
+}
---
+++ C_TEST/K_TEST_20240124_Near/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_TEST_20240124_Near
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result_array = solution("aabbccddabc");
+
+            string result = "";
+            for (int i = 0; i < result_array.Length; i++) {
+                result += result_array[i]+"";
+            }
+
+            Console.WriteLine(result);
+        }
+        static int[] solution(string s)
+        {
+            int[] answer = new int[s.Length];
+
+            int index = -1;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                index = -1; //없으면 -1
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (s[i] == s[j])
+                    {
+                        index = i - j; //있으면 i-j
+                        break;
+                    }
+                }
+                answer[i] = index;
+            }
+
+            return answer;
+        }
+    }
+}
